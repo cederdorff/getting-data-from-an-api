23f7e3663cf2533a014811b6f2df9cc95cd1d690
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-12-23 19:12:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // https://dummyjson.com/ er en API hvor man kan tilgå testdata
 // fx https://dummyjson.com/users, https://dummyjson.com/posts eller https://dummyjson.com/products
 function getData() {
-    fetch("https://dummyjson.com/users") // fetch'er/ henter fra data fra https://dummyjson.com/users
+    fetch("http://localhost:3000/users") // fetch'er/ henter fra data fra https://dummyjson.com/users
         .then(response => response.json()) // laver data om fra JSON til JavaScript data (objekter og arrays)
         .then(data => {
             console.log(data); // logger data i console
@@ -24,6 +24,7 @@ function displayData(listOfUsers) {
         document.querySelector("#users-grid").innerHTML += /*html*/ `
             <article>
                 <img src="${user.image}"/>
+                <h2>${user.firstName} ${user.lastName}</h2>
                 <p>${user.age} years old</p>
             </article>
         `;
